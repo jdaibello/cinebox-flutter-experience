c00ee1bae3fefe62780ab88e52e780f53f9bff85
João Pedro <jpedrodaibello1999@outlook.com>
2025-08-13 09:41:18 -0300

    feat: implement app theme with custom colors and text styles

---
+++ lib/ui/core/themes/colors.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/widgets.dart';
+
+abstract class AppColors {
+  static const red = Color(0xFFEA4335);
+  static const backgroundColor = Color(0xFFF9F9F9);
+  static const lightGrey = Color(0xFF989898);
+  static const darkGrey = Color(0xFF4F4F4F);
+  static const yellow = Color(0xFFFFBA49);
+}
