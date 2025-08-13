eb3d6d62b1b757ae7b9ec0322ef2ade4b266ed65
João Pedro <jpedrodaibello1999@outlook.com>
2025-08-13 16:45:30 -0300

    feat: add splash screen UI with background image and logo overlay

--- lib/ui/splash/splash_screen.dart
+++ lib/ui/splash/splash_screen.dart
@@ -1,3 +1,4 @@
+import 'package:cinebox/ui/core/themes/resource.dart';
 import 'package:cinebox/ui/core/widgets/loader_messages.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
@@ -13,10 +14,23 @@ class _SplashScreenState extends ConsumerState<SplashScreen> with LoaderAndMessa
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Splash Screen'),
+      body: Stack(
+        children: [
+          Image.asset(
+            R.ASSETS_IMAGES_BG_LOGIN_PNG,
+            width: double.infinity,
+            height: double.infinity,
+            fit: BoxFit.cover,
+          ),
+          Container(
+            constraints: BoxConstraints.expand(),
+            color: Colors.black.withAlpha(170),
+          ),
+          Center(
+            child: Image.asset(R.ASSETS_IMAGES_LOGO_PNG),
+          ),
+        ],
       ),
-      body: Container(),
     );
   }
 }
